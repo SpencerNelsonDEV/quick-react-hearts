21502a99fd589323b8b4727623ac04ec19fb4e4f
Spencer Nelson <spencernelsondev@gmail.com>
2021-09-02 10:51:47 -0700

    Added Array.from and .map() method

--- src/components/RandomHearts.js
+++ src/components/RandomHearts.js
@@ -5,12 +5,24 @@ const randomNum = () => {
   return Math.floor(Math.random() * 500) + 50;
 };
 
+const randomNumArray = Array.from({ length: 3 }, (element, index) => {
+  const top = randomNum();
+  const left = randomNum();
+  return {
+    top,
+    left,
+  };
+});
+
+const numLeft = randomNumArray.map((num) => num.left);
+const numTop = randomNumArray.map((num) => num.top);
+
 const RandomHearts = () => {
   return (
     <div>
-      <Heart top={randomNum()} left={randomNum()} />
-      <Heart top={randomNum()} left={randomNum()} />
-      <Heart top={randomNum()} left={randomNum()} />
+      <Heart top={numTop[0]} left={numLeft[0]} />
+      <Heart top={numTop[1]} left={numLeft[1]} />
+      <Heart top={numTop[2]} left={numLeft[2]} />
     </div>
   );
 };
